691686cc8a25b726915073ddcfea1b9925b352cd
Saras355 <saraswati.widhisari355@gmail.com>
2023-09-27 08:52:10 +0700

    tugas 4

--- main/models.py
+++ main/models.py
@@ -1,11 +1,30 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 # Create your models here.
-from django.db import models
+
 
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
-    price = models.IntegerField()
-    amount = models.IntegerField()
+    price = models.IntegerField(
+        validators=[MinValueValidator(0)]
+    )
+    amount = models.IntegerField(
+        validators=[MinValueValidator(0)]
+    )
     description = models.TextField()
+
+    #method untuk tambah jumlah stok produk sebanyak satu item
+    def add_product(self, quantity):
+        self.amount += quantity
+        self.save()
+    #method untuk mengurangi jumlah stok produk sebanyak satu item
+    def subtract_product(self, quantity):
+        if self.amount >= quantity:
+            self.amount -= quantity
+            self.save()
+
+    def __str__(self):
+        return self.name
